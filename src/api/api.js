040c54100be48ea612378bd9a189f72431880e19
Michelle Mason <michellemason95@yahoo.com>
2023-05-11 18:29:23 +0100

    initial but late commit

---
+++ src/api/api.js
@@ -0,0 +1,88 @@
+import axios from "axios";
+
+const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
+
+/** API Class.
+ *
+ * Static class tying together methods used to get/send to to the API.
+ * There shouldn't be any frontend-specific stuff here, and there shouldn't
+ * be any API-aware stuff elsewhere in the frontend.
+ *
+ */
+
+class PuphubApi {
+  // the token for interactive with the API will be stored here.
+  static token;
+
+  static async request(endpoint, data = {}, method = "get") {
+    console.debug("API Call:", endpoint, data, method);
+
+    const url = `${BASE_URL}/${endpoint}`;
+    const headers = { Authorization: `Bearer ${PuphubApi.token}` };
+    const params = (method === "get")
+        ? data
+        : {};
+
+    try {
+      return (await axios({ url, method, data, params, headers })).data;
+    } catch (err) {
+      console.error("API Error:", err.response);
+      let message = err.response.data.error.message;
+      throw Array.isArray(message) ? message : [message];
+    }
+  }
+
+  // Individual API routes
+
+  /** Get the current user. */
+
+  static async getCurrentUser(username) {
+    let res = await this.request(`users/${username}`);
+    return res.user;
+  }
+
+  /** Get dogs (filtered by name if not undefined) */
+
+  static async getDogs(name) {
+    let res = await this.request("dogs", { name });
+    return res.dogs;
+  }
+
+  /** Get details on a dog by id. */
+
+  static async getDog(id) {
+    let res = await this.request(`dogs/${id}`);
+    return res.dogs;
+  }
+
+  /** Get list of bookings (filtered by date if not undefined) */
+
+  static async getBookings(date) {
+    let res = await this.request("bookings", { date });
+    return res.date;
+  }
+
+  /** Get token for login from username, password. */
+
+  static async login(data) {
+    let res = await this.request(`auth/token`, data, "post");
+    return res.token;
+  }
+
+  /** Signup for site. */
+
+  static async signup(data) {
+    let res = await this.request(`auth/register`, data, "post");
+    return res.token;
+  }
+
+  /** Save user profile page. */
+
+  static async saveProfile(username, data) {
+    let res = await this.request(`users/${username}`, data, "patch");
+    return res.user;
+  }
+}
+
+ 
+export default PuphubApi;
